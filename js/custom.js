128e1fe044a387e2e09dbe93b12e6c9a65192f9b
edwincarbajal <exaviercarbajal@gmail.com>
2017-07-12 15:09:00 -0400

    Refactor code

--- js/custom.js
+++ js/custom.js
@@ -85,7 +85,7 @@ $(function() {
       }
     }
     $('.add-link-button').attr('disabled', 'disabled');
-    $('.add-link-button').removeClass('dark_button').addClass('disabled_button')
+    $('.add-link-button').addClass('disabled_button');
   });
 
   // copy cards link when clicked
